d305163189ca1ddc5ea68c66d8716232088f48d3
Nityanand Yadav <nityanandyadav2324@gmail.com>
2025-03-06 10:01:45 +0530

    new commits

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-        // Warning: This allows production builds to successfully complete
-        // even if your project has ESLint errors.
         ignoreDuringBuilds: true,
     },
     images: {
@@ -12,6 +10,19 @@ const nextConfig = {
                 hostname: 'images.unsplash.com',
             },
         ],
+    },
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",  // Apply to all API routes
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "https://typeblaze.vercel.app/" }, // Update with your deployed URL
+                    { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                ],
+            },
+        ];
     },
 };
 
